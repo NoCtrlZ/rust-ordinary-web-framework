7c2588a75ca3c4a5694169f8ce06ae2047e84daf
Hemlocked <phantomofrotten@gmail.com>
2019-11-10 02:10:01 +0900

    parse http request function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
 
 fn instance_listen(port: &str) {
     let listener = TcpListener::bind(format!("localhost{}", port)).unwrap();
 
     for stream in listener.incoming() {
+
         let stream = stream.unwrap();
 
-        println!("TCP Connection");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
 fn main() {
