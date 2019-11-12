13ca85450a1332263d9823ac542ab4c887380e10
Hemlocked <phantomofrotten@gmail.com>
2019-11-12 16:36:53 +0900

    divide request into header and body

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,19 @@
 use std::net::TcpStream;
 use std::io::prelude::*;
 
+struct Request {
+    method: String,
+    path: String,
+    proto: String,
+    host: String,
+    header: String,
+    body: String,
+}
+
 pub fn parse_request(stream: TcpStream) {
-    let request = trim_request(stream);
-    println!("{:?}", request);
+    let trimed_request = trim_request(stream);
+    let request = arrange_request(trimed_request);
+    println!("{:?}", request.method);
 }
 
 fn trim_request(mut stream: TcpStream) -> String {
@@ -11,4 +21,24 @@ fn trim_request(mut stream: TcpStream) -> String {
     stream.read(&mut buffer).unwrap();
     let request = String::from_utf8_lossy(&buffer[..]);
     request.trim_matches(char::from(0)).to_string()
+}
+
+fn arrange_request(mut request: String) -> Request {
+    let (head, body) = divide_request(request);
+    println!("{:?}", head);
+    println!("{:?}", body);
+    let req = Request {
+        method: "String".to_string(),
+        path: "String".to_string(),
+        proto: "String".to_string(),
+        host: "String".to_string(),
+        header: "String".to_string(),
+        body: "String".to_string(),
+    };
+    req
+}
+
+fn divide_request(mut request: String) -> (String, String) {
+    let v: Vec<&str> = request.split("\r\n\r\n").collect();
+    (v[0].to_string(), v[1].to_string())
 }
