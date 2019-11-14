71f99ea71be17540e4f21c03c2f061f0d00b9e16
Hemlocked <phantomofrotten@gmail.com>
2019-11-14 16:45:22 +0900

    execute function to request

--- src/response.rs
+++ src/response.rs
@@ -2,5 +2,7 @@ use crate::router;
 use std::collections::HashMap;
 
 pub fn response_for_request(path: String, url: String, handler: HashMap<String, HashMap<String, fn()>>) {
-    println!("{}", path);
+    let func = handler.get(&path).and_then(|m| m.get(&url)).unwrap();
+    println!("{:?}", func);
+    func();
 }
